3abb2f67387ade39c0029f753cf97d9df1d04306
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-01-11 14:28:37 +0100

    docs: add missing @param

--- R/api.R
+++ R/api.R
@@ -1,12 +1,3 @@
-bb_make_request <- function(resource) {
-  request("https://api.statistiken.bundesbank.de/rest") |>
-    req_user_agent("worldbank (https://m-muecke.github.io/worldbank)") |>
-    req_url_path_append(resource) |>
-    req_error(body = \(resp) resp_body_json(resp)$title) |>
-    req_perform() |>
-    resp_body_xml()
-}
-
 #' Returns data for a given flow and key
 #'
 #' @param flow character(1) flow to query
@@ -39,6 +30,46 @@ bb_data <- function(flow, key = NULL) {
   as_tibble(data)
 }
 
+#' Returns available data structures
+#'
+#' @param id character(1) id to query. Default `NULL`.
+#' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-metadata>
+#' @family metadata
+#' @export
+bb_data_structure <- function(id = NULL) {
+  bb_metadata("metadata/datastructure/BBK", id)
+}
+
+#' Returns available dataflows
+#'
+#' @param id character(1) id to query. Default `NULL`.
+#' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-metadata>
+#' @family metadata
+#' @export
+bb_dataflow <- function(id = NULL) {
+  bb_metadata("metadata/dataflow/BBK", id)
+}
+
+#' Returns available code lists
+#'
+#' @param id character(1) id to query. Default `NULL`.
+#' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-metadata>
+#' @family metadata
+#' @export
+bb_codelist <- function(id = NULL) {
+  bb_metadata("metadata/codelist/BBK", id)
+}
+
+#' Returns available concepts
+#'
+#' @param id character(1) id to query. Default `NULL`.
+#' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-metadata>
+#' @family metadata
+#' @export
+bb_concept_scheme <- function(id = NULL) {
+  bb_metadata("metadata/conceptscheme/BBK", id)
+}
+
 bb_metadata <- function(resource, id = NULL) {
   stopifnot(is.null(id) || is.character(id) && length(id) == 1)
   # only supports xml return format
@@ -49,38 +80,11 @@ bb_metadata <- function(resource, id = NULL) {
   bb_make_request(resource)
 }
 
-#' Returns available data structures
-#'
-#' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-metadata>
-#' @family metadata
-#' @export
-bb_data_structure <- function(id = NULL) {
-  bb_metadata("metadata/datastructure/BBK", id)
-}
-
-#' Returns available dataflows
-#'
-#' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-metadata>
-#' @family metadata
-#' @export
-bb_dataflow <- function(id = NULL) {
-  bb_metadata("metadata/dataflow/BBK", id)
-}
-
-#' Returns available code lists
-#'
-#' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-metadata>
-#' @family metadata
-#' @export
-bb_codelist <- function(id = NULL) {
-  bb_metadata("metadata/codelist/BBK", id)
-}
-
-#'
-#'
-#' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-metadata>
-#' @family metadata
-#' @export
-bb_concept_scheme <- function(id = NULL) {
-  bb_metadata("metadata/conceptscheme/BBK", id)
+bb_make_request <- function(resource) {
+  request("https://api.statistiken.bundesbank.de/rest") |>
+    req_user_agent("worldbank (https://m-muecke.github.io/worldbank)") |>
+    req_url_path_append(resource) |>
+    req_error(body = \(resp) resp_body_json(resp)$title) |>
+    req_perform() |>
+    resp_body_xml()
 }

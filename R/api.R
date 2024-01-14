b75dc3b6de2ec1572ac2de4cb571aeab7967933d
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-01-14 11:44:05 +0100

    feat: initial support for start and end period

--- R/api.R
+++ R/api.R
@@ -2,12 +2,37 @@
 #'
 #' @param flow character(1) flow to query
 #' @param key character(1) key to query. Default `NULL`.
+#' @param start_period character(1) start date of the data. Supported formats:
+#'   - YYYY for annual data (e.g., "2019")
+#'   - YYYY-S[1-2] for semi-annual data (e.g., "2019-S1")
+#'   - YYYY-Q[1-4] for quarterly data (e.g., "2019-Q1")
+#'   - YYYY-MM for monthly data (e.g., "2019-01")
+#'   - YYYY-W[01-53] for weekly data (e.g., "2019-W01")
+#'   - YYYY-MM-DD for daily and business data (e.g., "2019-01-01")
+#'   If `NULL`, no start date restriction is applied (data retrieved from the
+#'   earliest available date). Default `NULL`.
+#' @param end_period character(1) end date of the data, in the same format as
+#'   start_period. If `NULL`, no end date restriction is applied (data
+#'   retrieved up to the most recent available date). Default `NULL`.
 #' @references <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/web-service-interface-data>
 #' @family data
 #' @export
 #' @examples
+#' # fetch all data for a given flow and key
 #' bb_data("BBSIS", "D.I.ZST.ZI.EUR.S1311.B.A604.R10XX.R.A.A._Z._Z.A")
-bb_data <- function(flow, key = NULL) {
+#'
+#' # specified period (start date-end date) for daily data
+#' bb_data(
+#'   "BBSIS", "D.I.ZST.ZI.EUR.S1311.B.A604.R10XX.R.A.A._Z._Z.A",
+#'   start_period = "2020-01-01",
+#'   end_period = "2020-08-01"
+#' )
+#' # or only specify the start date
+#' bb_data(
+#'   "BBSIS", "D.I.ZST.ZI.EUR.S1311.B.A604.R10XX.R.A.A._Z._Z.A",
+#'   start_period = "2020-01-01"
+#' )
+bb_data <- function(flow, key = NULL, start_period = NULL, end_period = NULL) {
   stopifnot(is.character(flow), is.null(key) || is.character(key))
   flow <- toupper(flow)
   if (is.null(key)) {
@@ -16,7 +41,11 @@ bb_data <- function(flow, key = NULL) {
     key <- toupper(key)
     resource <- sprintf("data/%s/%s", flow, key)
   }
-  body <- bb_make_request(resource)
+  body <- bb_make_request(
+    resource = resource,
+    startPeriod = start_period,
+    endPeriod = end_period
+  )
 
   entries <- body |>
     xml2::xml_find_all("//generic:Obs[generic:ObsValue]")
@@ -83,7 +112,7 @@ bb_concept_scheme <- function(id = NULL) {
 bb_error_body <- function(resp) {
   body <- resp_body_json(resp)
   message <- body$title
-  docs <- "See docs at <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/status-codes/status-codes-855918>"
+  docs <- "See docs at <https://www.bundesbank.de/en/statistics/time-series-databases/help-for-sdmx-web-service/status-codes/status-codes-855918>" # nolint
   c(message, docs)
 }
 
@@ -97,11 +126,12 @@ bb_metadata <- function(resource, id = NULL) {
   bb_make_request(resource)
 }
 
-bb_make_request <- function(resource) {
+bb_make_request <- function(resource, ...) {
   request("https://api.statistiken.bundesbank.de/rest") |>
     req_user_agent("worldbank (https://m-muecke.github.io/worldbank)") |>
     req_headers(`Accept-Language` = "en") |>
     req_url_path_append(resource) |>
+    req_url_query(...) |>
     req_error(body = bb_error_body) |>
     req_perform() |>
     resp_body_xml()
--- R/bundesbank-package.R
+++ R/bundesbank-package.R
@@ -6,6 +6,7 @@
 #' @importFrom httr2 req_headers
 #' @importFrom httr2 req_perform
 #' @importFrom httr2 req_url_path_append
+#' @importFrom httr2 req_url_query
 #' @importFrom httr2 req_user_agent
 #' @importFrom httr2 request
 #' @importFrom httr2 resp_body_json

4e25873d80fa579a9a7ada6fbf0974c8de4fc483
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-05-01 12:55:05 +0200

    refactor: move assertions to own file

---
+++ R/assertions.R
@@ -0,0 +1,20 @@
+is_character <- function(x) {
+  is.character(x) && !anyNA(x) && length(x) > 0L
+}
+
+is_string <- function(x) {
+  is.character(x) && length(x) == 1L && !is.na(x)
+}
+
+is_string_or_null <- function(x) {
+  is.null(x) || is_string(x)
+}
+
+is_count <- function(x) {
+  is.numeric(x) && length(x) == 1L && !is.na(x) &&
+    as.integer(x) == x && x > 0L
+}
+
+is_count_or_null <- function(x) {
+  is.null(x) || is_count(x)
+}
--- R/utils.R
+++ R/utils.R
@@ -14,27 +14,6 @@ as_tibble <- function(x) {
   }
 }
 
-is_character <- function(x) {
-  is.character(x) && !anyNA(x) && length(x) > 0L
-}
-
-is_string <- function(x) {
-  is.character(x) && length(x) == 1L && !is.na(x)
-}
-
-is_string_or_null <- function(x) {
-  is.null(x) || is_string(x)
-}
-
-is_count <- function(x) {
-  is.numeric(x) && length(x) == 1L && !is.na(x) &&
-    as.integer(x) == x && x > 0L
-}
-
-is_count_or_null <- function(x) {
-  is.null(x) || is_count(x)
-}
-
 na_if_empty <- function(x, empty = "") {
   replace(x, x == empty, NA_character_)
 }
